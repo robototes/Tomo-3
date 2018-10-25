464b43710ef01010cfa6cbb063435bc4626188fd
U-DESKTOP-ECKNV19\Condor <alexander.bman@hotmail.com>
2018-10-25 16:21:26 -0700

    Added MoveCannonDownCommand.

---
+++ src/org/usfirst/frc/team2412/robot/commands/MoveCannonDownCommand.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team2412.robot.commands;
+
+public class MoveCannonDownCommand extends CommandBase {
+	
+	public MoveCannonDownCommand() {
+		requires(moveCannon);
+	}
+	
+	/** Called when the command is run */
+	@Override
+	protected void execute() {
+		moveCannon.moveCannonDown();
+	}
+	
+	/** Determines when this command exits */
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+	
+	/** Called when this command ends */
+	@Override
+	protected void end() {
+		moveCannon.stopCannon();
+	}
+}
