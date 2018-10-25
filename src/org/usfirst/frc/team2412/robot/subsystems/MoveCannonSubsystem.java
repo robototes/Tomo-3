aa4619cdfb9bfd447f57e5707a343d545e97c381
U-DESKTOP-ECKNV19\Condor <alexander.bman@hotmail.com>
2018-10-25 16:08:06 -0700

    Added MoveCannonSubsystem.

---
+++ src/org/usfirst/frc/team2412/robot/subsystems/MoveCannonSubsystem.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team2412.robot.subsystems;
+
+import org.usfirst.frc.team2412.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class MoveCannonSubsystem extends Subsystem {
+	/** Victor SP instance for moving the cannon up and down */
+	private VictorSP moveCannonVictor = RobotMap.movingCannonVictor;
+	
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+	}
+	
+	/** Method for moving the cannon up */
+	public void moveCannonUp() {
+		moveCannonVictor.set(0.3);
+	}
+	
+	/** Method for moving the cannon down */
+	public void moveCannonDown() {
+		moveCannonVictor.set(-0.3);
+	}
+}
