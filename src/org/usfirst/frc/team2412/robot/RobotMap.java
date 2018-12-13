8c76109d30fbcdca5ae1278ae1c50236cc3a2b89
Robototes Programmers <programmers@robototes.com>
2018-12-13 15:26:19 -0800

    Revert "Temporarily removed pneumatics for now."
    This reverts commit df988d3f75eaae4a75ee9d54ccf0a433fab5455e.

--- src/org/usfirst/frc/team2412/robot/RobotMap.java
+++ src/org/usfirst/frc/team2412/robot/RobotMap.java
@@ -74,10 +74,10 @@ public class RobotMap {
 	};
 	
 	/** Compressor instances for shooting */
-//	public static Compressor[] shootingCompressors = new Compressor[] {
-//		new Compressor(shootingCompressorIDs[0]),
-//		new Compressor(shootingCompressorIDs[1])
-//	};
+	public static Compressor[] shootingCompressors = new Compressor[] {
+		new Compressor(shootingCompressorIDs[0]),
+		new Compressor(shootingCompressorIDs[1])
+	};
 	
 	/** Victor SP ID for moving the cannon */
 	public static int movingCannonVictorID = 5;
